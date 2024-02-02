2bdcc2a34d0e3f292042e4b93018aba61b55eab0
sssseungk <cacy0118@naver.com>
2024-02-02 16:43:55 +0900

    🎨 임의의 데이터에 id 추가 및 스타일링 수정

--- src/App.js
+++ src/App.js
@@ -6,23 +6,26 @@ import MovieList from './MovieList';
 
 const dummyList = [
   {
+    id: 1,
     title: "웡카",
     date: "2024년 01월 31일 10:00",
-    theater: "롯데시네마 동탄",
+    theater: "롯데시네마 월드타워",
     seat: "G14",
     star: "⭐⭐⭐",
   },
   {
+    id: 2,
     title: "시민덕희",
     date: "2024년 01월 28일 10:10",
-    theater: "롯데시네마 동탄",
+    theater: "롯데시네마 용산",
     seat: "F04",
     star: "⭐⭐",
   },
   {
+    id: 3,
     title: "너의 이름은",
     date: "2024년 01월 24일 12:25",
-    theater: "CGV 동탄",
+    theater: "CGV 용산아이파크몰",
     seat: "G11",
     star: "⭐⭐⭐⭐⭐",
   }
