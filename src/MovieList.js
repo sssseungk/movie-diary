59040eb66888f359ca3caaae9eafacba00789965
sssseungk <cacy0118@naver.com>
2024-02-02 16:44:51 +0900

    🎨 리스트 렌더링 리스트 아이템에 key 추가

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
 function MovieList({movieData}) {
-  console.log(movieData);
   return (
     <ul className='movieList'>
       {movieData.map((it) => 
-        <li>
+        <li key={it.id}>
           <strong>{it.title}</strong>
           <span>{it.date}</span>
           <span>{it.theater}</span>
