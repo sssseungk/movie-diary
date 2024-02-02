a15c8a5036f3214d8fc601dd0612e478a295825e
sssseungk <cacy0118@naver.com>
2024-02-02 14:48:38 +0900

    🎨 컴포넌트 분리 및 임의의 영화 데이터 생성

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import './App.css';
+import MovieWrite from './MovieWrite';
+import MovieNav from './MovieNav';
+import MovieList from './MovieList';
 
-import React from 'react'
-import MovieTitle from './MovieTitle';
-import MovieDetail from './MovieDetail';
+
+const dummyList = [
+  {
+    title: "웡카",
+    date: "2024년 01월 31일 10:00",
+    theater: "롯데시네마 동탄",
+    seat: "G14",
+    star: "⭐⭐⭐",
+  },
+  {
+    title: "시민덕희",
+    date: "2024년 01월 28일 10:10",
+    theater: "롯데시네마 동탄",
+    seat: "F04",
+    star: "⭐⭐",
+  },
+  {
+    title: "너의 이름은",
+    date: "2024년 01월 24일 12:25",
+    theater: "CGV 동탄",
+    seat: "G11",
+    star: "⭐⭐⭐⭐⭐",
+  }
+]
+
 
 function App() {
+
   return (
     <div className='movieDiary'>
-      <MovieTitle/>
-      <MovieDetail/>
+      <MovieWrite/>
+      <MovieNav/>
+      <MovieList movieData={dummyList}/>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/MovieWrite.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MovieTitle from './MovieTitle'
+import MovieDetail from './MovieDetail'
+
+function MovieWrite() {
+  return (
+    <div className='movieWrite'>
+      <MovieTitle/>
+      <MovieDetail/>
+    </div>
+  )
+}
+
+export default MovieWrite
