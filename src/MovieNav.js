af185f4e7952117e0fd8c87b2a7ee90b235cc188
sssseungk <cacy0118@naver.com>
2024-02-02 14:47:46 +0900

    🔨 movieNav 컴포넌트 생성

---
+++ src/MovieNav.js
@@ -0,0 +1,17 @@
+
+function MovieNav() {
+  return (
+    <nav className="movieNav">
+      <ul>
+        <li>
+          <a href="#">영화</a>
+        </li>
+        <li>
+          <a href="#">책</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default MovieNav;
