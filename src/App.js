47c4a4d30cd3f8ad5fd8abf2ae45b07631858f34
sssseungk <cacy0118@naver.com>
2024-02-02 13:20:39 +0900

    🔨 관람 정보 입력 컴포넌트 생성 및 스타일링

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 
 import React from 'react'
 import MovieTitle from './MovieTitle';
+import MovieDetail from './MovieDetail';
 
 function App() {
   return (
     <div className='movieDiary'>
       <MovieTitle/>
+      <MovieDetail/>
     </div>
   )
 }
