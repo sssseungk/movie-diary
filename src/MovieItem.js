927c242c48cc717307280f3600ef77a63eaec8da
sssseungk <cacy0118@naver.com>
2024-02-04 15:14:40 +0900

    💄 사용자가 평가한 별점을 숫자가 아닌 별 이모티콘으로 렌더링 하도록 하는 함수 추가

--- src/MovieItem.js
+++ src/MovieItem.js
@@ -1,11 +1,18 @@
 function MovieItem({id, title, date, theater, seat, star}) {
+
+  // 사용자가 입력한 별점 별 이모티콘으로 표현하는 함수
+  const renderStar = (count) => {
+    const stars = '⭐'.repeat(count);
+    return stars;
+  }
+
   return (
     <li key={id}>
       <strong>{title}</strong>
       <span>{date}</span>
       <span>{theater}</span>
       <span>{seat}</span>
-      <span>{star}</span>
+      <span>{renderStar(star)}</span>
     </li>
   )
 }
